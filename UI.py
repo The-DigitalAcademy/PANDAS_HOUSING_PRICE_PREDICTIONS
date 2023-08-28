d566a1f79864a39c8592f876e52f14d4253e43d3
SIBONGILE MOKOENA <132346739+sibongile029@users.noreply.github.com>
2023-08-28 09:05:59 +0200

    Update UI.py

--- UI.py
+++ UI.py
@@ -1,31 +1,21 @@
 import pandas as pd
 import streamlit as st
 import joblib
-from sklearn.naive_bayes import GaussianNB
-from sklearn.tree import DecisionTreeClassifier
 
-# Function to load the models
-def load_models():
-    model2_path = '/content/model2.pkl'  # Corrected file extension to '.pkl'
-    model3_path = '/content/model3.pkl'  # Corrected file extension to '.pkl'
-    
-    model2 = joblib.load(model2_path)    
-    model3 = joblib.load(model3_path)
-    
-    return model2, model3
+# Load the saved models
+model2 = joblib.load('model2.pkl')
+model3 = joblib.load('model3.pkl')
 
 def main():
     # Title of the web app
     st.title("Model Predictions App")
     st.write("Enter the following features to get predictions:")
 
-    # Load the models
-    model2, model3 = load_models()
-
     # User input for features
     st.header('Feature Input')
-    
+
     # Create input fields for each feature
+    # For numerical features, use st.number_input()
     feature1 = st.number_input("longitude", value=0)
     feature2 = st.number_input("latitude", value=0)
     feature3 = st.number_input("housing_median_age", value=0)
@@ -34,10 +24,10 @@ def main():
     feature6 = st.number_input("households", value=0)
     feature7 = st.number_input("median_income", value=0)
     feature8 = st.number_input("median_house_value", value=0)
-    feature9 = st.selectbox("ocean_proximity", ["<1H OCEAN", "INLAND", "NEAR OCEAN", "NEAR BAY", "ISLAND"])  # Dropdown for ocean_proximity
-    
+    feature9 = st.selectbox("ocean_proximity", ["<1H OCEAN", "INLAND", "NEAR OCEAN", "NEAR BAY", "ISLAND"])
+
     # Selection box for the model to use
-    selected_model = st.selectbox("Choose a model", ["model2", "model3"])  # Corrected typo
+    selected_model = st.selectbox("Choose a model", ["model2", "model3"])
 
     # Button for predictions
     clicked = st.button('Get Predictions')
@@ -45,9 +35,12 @@ def main():
     # Perform predictions when the button is clicked
     if clicked:
         if selected_model == "model2":
-            model = model2  # Corrected variable name
+            model = model3
         elif selected_model == "model3":
-            model = model3  # Corrected variable name
+            model = model2
+        else:
+            st.warning("Please select a valid model.")
+            return
 
         # Perform predictions using the selected model
         input_features = pd.DataFrame({
@@ -69,3 +62,4 @@ def main():
 
 if __name__ == '__main__':
     main()
+
