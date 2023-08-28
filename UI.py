57bbc11c22396973ecb32d0fc9fc72ddffdf21c0
SIBONGILE MOKOENA <132346739+sibongile029@users.noreply.github.com>
2023-08-28 10:46:09 +0200

    Update UI.py

--- UI.py
+++ UI.py
@@ -19,11 +19,12 @@ def main():
     feature1 = st.number_input("longitude", value=0)
     feature2 = st.number_input("latitude", value=0)
     feature3 = st.number_input("housing_median_age", value=0)
-    feature4 = st.number_input("total_bedrooms", value=0)
-    feature5 = st.number_input("population", value=0)
-    feature6 = st.number_input("households", value=0)
-    feature7 = st.number_input("median_income", value=0)
-    feature8 = st.selectbox("ocean_proximity", ["<1H OCEAN", "INLAND", "NEAR OCEAN", "NEAR BAY", "ISLAND"])
+    feature4 = st.number_input("total_rooms", value=0)
+    feature5 = st.number_input("total_bedrooms", value=0)
+    feature6 = st.number_input("population", value=0)
+    feature7 = st.number_input("households", value=0)
+    feature8 = st.number_input("median_income", value=0)
+    feature9 = st.selectbox("ocean_proximity", ["<1H OCEAN", "INLAND", "NEAR OCEAN", "NEAR BAY", "ISLAND"])
 
     # Selection box for the model to use
     selected_model = st.selectbox("Choose a model", ["model2", "model3"])
@@ -50,11 +51,12 @@ def main():
                 "longitude": [feature1],
                 "latitude": [feature2],
                 "housing_median_age": [feature3],
-                "total_bedrooms": [feature4],
-                "population": [feature5],
-                "households": [feature6],
-                "median_income": [feature7],
-                "ocean_proximity": [feature8]
+                "total_rooms": [feature4],               
+                "total_bedrooms": [feature5],
+                "population": [feature6],
+                "households": [feature7],
+                "median_income": [feature8],
+                "ocean_proximity": [feature9]
             })
 
             if hasattr(model, 'predict'):  # Check if the model has a predict method
