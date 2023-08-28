5287ed3d578c5912c6809471d67c6c573c7dd736
SIBONGILE MOKOENA <132346739+sibongile029@users.noreply.github.com>
2023-08-28 10:49:07 +0200

    Update main.py

--- main.py
+++ main.py
@@ -12,17 +12,20 @@ feature1 = st.number_input("longitude", value=0)
 feature2 = st.number_input("latitude", value=0)
 feature3 = st.number_input("housing_median_age", value=0)
 feature4 = st.number_input("total_bedrooms", value=0)
-feature5 = st.number_input("population", value=0)
-feature6 = st.number_input("households", value=0)
-feature7 = st.number_input("median_income", value=0)
-feature8 = st.number_input("median_house_value", value=0)
+feature5 = st.number_input("total_rooms", value=0)  # Add total_rooms as feature 5
+feature6 = st.number_input("population", value=0)
+feature7 = st.number_input("households", value=0)
+feature8 = st.number_input("median_income", value=0)
+feature9 = st.selectbox("ocean_proximity", ["<1H OCEAN", "INLAND", "NEAR OCEAN", "NEAR BAY", "ISLAND"])
 
 # Make predictions when a button is clicked
 if st.button("Predict"):
     try:
         # Prepare the input data for prediction
         input_data = np.array([feature1, feature2, feature3, feature4, feature5,
-                               feature6, feature7, feature8])
+                               feature6, feature7, feature8, feature9])
+
+        # Convert ocean_proximity to a one-hot encoded vector if needed
 
         st.write("Input Data:", input_data)  # Log the input data
 
