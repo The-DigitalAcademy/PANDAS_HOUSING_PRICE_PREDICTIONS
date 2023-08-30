18d1512dd1af5fe4ee02a12206db111e7adde051
SIBONGILE MOKOENA <132346739+sibongile029@users.noreply.github.com>
2023-08-30 15:57:31 +0200

    Update main.py

--- main.py
+++ main.py
@@ -2,56 +2,101 @@ import streamlit as st
 import tensorflow as tf
 import numpy as np
 import pandas as pd
+from sklearn.preprocessing import StandardScaler
+import joblib
+from PIL import Image
 
-# Load the pre-trained model
-model = tf.keras.models.load_model('model2.h5')
+# Load your trained model (replace with the path to your model file)
+model = tf.keras.models.load_model('model3(1).h5')
 
-st.title("Loan Approval Prediction")
+# Load your scaler (replace with the path to your scaler file)
+scaler = joblib.load('scaler.pkl')
 
-# Input fields for the new features
-feature1 = st.number_input("longitude", value=0)
-feature2 = st.number_input("latitude", value=0)
-feature3 = st.number_input("housing_median_age", value=0)
-feature4 = st.number_input("total_bedrooms", value=0)
-feature5 = st.number_input("total_rooms", value=0)  # Add total_rooms as feature 5
-feature6 = st.number_input("population", value=0)
-feature7 = st.number_input("households", value=0)
-feature8 = st.number_input("median_income", value=0)
-feature9 = st.selectbox("ocean_proximity", ["<1H OCEAN", "INLAND", "NEAR OCEAN", "NEAR BAY", "ISLAND"])
-# Convert ocean_proximity to a one-hot encoded vector
-# Convert ocean_proximity to a one-hot encoded vector
-ocean_proximity_encoded = pd.get_dummies([feature9], columns=["ocean_proximity"],drop_first=True).values
+# Define column names in the same order as your training data
+columns = ['longitude',	'latitude',	'housing_median_age',	'total_rooms',	'total_bedrooms',	'population',	'households',	'median_income',	'median_house_value',	'ocean_proximity']
+# Add more column names as needed to match your training data
 
-# Ensure ocean_proximity_encoded is a 2D array
-ocean_proximity_encoded = ocean_proximity_encoded.reshape(1, -1)
+# # Define a function to display the "Meet the Team" page
+# def meet_the_team():
+#     st.title("Meet the Team")
+    
+#     # Add team members with their pictures and descriptions
+#     team_members = [
+#         {"name": "Manoko Langa", "position": "Data Scientist", "image": "manoko.jpeg", "description": "Manoko is a data scientist with expertise in machine learning and data analysis."},
+#         {"name": "Thando Vilakazi", "position": "Web Developer", "image": "thando.jpeg", "description": "Thando is a web developer responsible for creating the Streamlit app."},
+#         # Add more team members as needed
+#     ]
+    
+#     # Create a container for the team members
+#     team_container = st.container()
+    
+#     # Create a CSS class for horizontal layout
+#     st.markdown(
+#         """
+#         <style>
+#         .horizontal-layout {
+#             display: flex;
+#             flex-direction: row;
+#         }
+#         </style>
+#         """,
+#         unsafe_allow_html=True,
+#     )
+    
+#     for member in team_members:
+#         with team_container:
+#             st.image(member['image'], caption=member['name'], use_column_width=True)
+#             st.markdown(f"**{member['name']}**")
+#             st.write(f"**Position**: {member['position']}")
+#             st.write(member['description'])
 
-# Remove the last column from input_data (ocean_proximity)
-#input_data = input_data[:, :-1]
+# Define a function to display the "Overview" page
+def project_overview():
+    
+# Set page configuration and title
+    st.title("Loan Approval Prediction")
+    st.title("Project Overview")
+    
+    st.write("This project is aimed at predicting loan approval using machine learning.")
 
+    st.write("It uses a deep learning model to predict whether a loan application is likely to be approved or not.")
+    
+    st.write("Please navigate to other pages for more details about the team and predictions.")
 
+# Set page configuration and title
+    #st.title("Loan Approval Prediction")
 
-# Make predictions when a button is clicked
-if st.button("Predict"):
-    try:
-        # Prepare the input data for prediction
-        input_data = [[feature1, feature2, feature3, feature4, feature5,
-                               feature6, feature7, feature8]]
-        #
-        input_data = input_data[:, :-1]
-        # Combine numerical features and the one-hot encoded categorical feature
-        input_data = np.concatenate([input_data, ocean_proximity_encoded], axis=1)
+# Sidebar
+with st.sidebar:
+    # Add options in the sidebar to navigate to different pages
+    page_selection = st.selectbox("Navigation", 
+                                   ["Project Overview", "Loan Approval Prediction"])
 
-        # Convert ocean_proximity to a one-hot encoded vector if needed
-
-        st.write("Input Data:", input_data)  # Log the input data
-
+# Main content
+if page_selection == "Loan Approval Prediction":
+    # Create a DataFrame from the input variables
+    input_df = pd.DataFrame(columns=columns)
+    input_df.loc[0] = [0] * len(columns)  # Initialize with zeros, you can replace these with your desired default values
+    
+    # Create input fields for each column
+    for column in columns:
+        input_df[column] = st.number_input(f"{column.replace('_', ' ').title()}", value=input_df[column].values[0])
+    
+    # Make predictions when a button is clicked
+    if st.button("Predict"):
+        # Standardize the input data using the loaded scaler
+        input_data = input_df.values  # Convert DataFrame to array
+        input_data = scaler.transform(input_data)
+        
         # Use the loaded model to make predictions
         prediction = model.predict(input_data)
+        result = "likely to be approved" if prediction >= 0.5 else "not likely to be approved"
+        
+        st.write(f"Prediction: {prediction[0]}")
+        st.write(f"The client is {result}.")
+# elif page_selection == "Meet the Team":
+#     meet_the_team()
+elif page_selection == "Project Overview":
+    project_overview()
 
-        st.write("Raw Prediction:", prediction)  # Log the raw prediction
-
-        # Display the prediction
-        st.write(f"Loan Approval Probability: {prediction[0, 0]}")
-    except Exception as e:
-        st.error("An error occurred during prediction.")
-        st.exception(e)  # Log the exception details
+        
