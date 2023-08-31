3580460d34a22f4a14fb3742e7834df10d34e8ce
Zandile_Mdiniso <132349003+ZandileMdiniso@users.noreply.github.com>
2023-08-31 20:25:28 +0200

    Create streamlit.py

---
+++ streamlit.py
@@ -0,0 +1,40 @@
+import streamlit as st
+import tensorflow as tf
+import numpy as np
+import pandas as pd
+
+# Load the pre-trained model
+model = tf.keras.models.load_model('model3.h5')
+
+# Load your actual dataset used for training
+df = pd.read_csv('/content/drive/MyDrive/houseing_clean.csv')
+
+# Preprocess user input
+scaler = StandardScaler()
+numeric_columns = ['housing_median_age', 'total_bedrooms', 'households', 'median_income']
+scaler.fit(df[numeric_columns])
+
+# Streamlit app
+st.title("HOUSE PRICE PREDICTION")
+
+# User input for features
+st.header('Feature Input')
+feature1 = st.number_input("housing_median_age", value=0)
+feature2 = st.number_input("total_bedrooms", value=0)
+feature3 = st.number_input("households", value=0)
+feature4 = st.number_input("median_income", value=0)
+
+# Scale and transform user input
+user_input = scaler.transform([[feature1, feature2, feature3, feature4]])
+
+# Button for predictions
+clicked = st.button('Get Predictions')
+
+# Perform predictions when the button is clicked
+if clicked:
+    # Perform predictions using the pre-trained model
+    prediction = model.predict(user_input)
+
+    # Display the prediction result
+    st.header('Prediction')
+    st.write(f'The predicted median house value is: {prediction[0][0]}')
