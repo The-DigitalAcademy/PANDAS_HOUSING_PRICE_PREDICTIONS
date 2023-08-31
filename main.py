c7a2c603d0d4d88be18e4fdad63bae55669d32c3
SIBONGILE MOKOENA <132346739+sibongile029@users.noreply.github.com>
2023-08-31 14:06:44 +0200

    Update main.py

--- main.py
+++ main.py
@@ -12,15 +12,11 @@ st.title("HOUSE PRICE PREDICTION")
 # User input for features
 st.header('Feature Input')
 
-feature1 = st.number_input("longitude", value=0)
-feature2 = st.number_input("latitude", value=0)
-feature3 = st.number_input("housing_median_age", value=0)
-feature4 = st.number_input("total_rooms", value=0)
-feature5 = st.number_input("total_bedrooms", value=0)
-feature6 = st.number_input("population", value=0)
-feature7 = st.number_input("households", value=0)
-feature8 = st.number_input("median_income", value=0)
-feature9 = st.selectbox("ocean_proximity", ["<1H OCEAN", "INLAND", "NEAR OCEAN", "NEAR BAY", "ISLAND"])
+feature1 = st.number_input("housing_median_age", value=0)
+feature2 = st.number_input("total_bedrooms", value=0)
+feature3 = st.number_input("households", value=0)
+feature4 = st.number_input("median_income", value=0)
+feature5 = st.selectbox("ocean_proximity", ["<1H OCEAN", "INLAND", "NEAR OCEAN", "NEAR BAY", "ISLAND"])
 
 # Button for predictions
 clicked = st.button('Get Predictions')
@@ -38,8 +34,7 @@ if clicked:
     feature9_encoded = ocean_proximity_mapping[feature9]
 
     # Prepare the input for prediction
-    input_features = np.array([[feature1, feature2, feature3, feature4, feature5,
-                                feature6, feature7, feature8, feature9_encoded]])
+    input_features = np.array([[feature1, feature2, feature3, feature4, feature5]])
 
     # Perform predictions using the selected model
     prediction = model.predict(input_features)
