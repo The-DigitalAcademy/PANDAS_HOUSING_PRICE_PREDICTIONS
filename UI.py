7d0c294d0d08db8a4284b8834673b73be0bbb159
SIBONGILE MOKOENA <132346739+sibongile029@users.noreply.github.com>
2023-09-01 13:02:17 +0200

    Update UI.py

--- UI.py
+++ UI.py
@@ -14,7 +14,7 @@ st.header('Feature Input')
 feature1 = st.number_input("housing_median_age", value=0)
 feature2 = st.number_input("total_bedrooms", value=0)
 feature3 = st.number_input("households", value=0)
-feature4 = st.number_input("median_income", value=0)
+feature4 = st.number_input("median_income", value=0.0)
 feature5 = st.selectbox("ocean_proximity", ["<1H OCEAN", "INLAND", "NEAR OCEAN", "NEAR BAY", "ISLAND"])
 
 # Button for predictions
