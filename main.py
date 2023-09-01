56385bf98399d6805eb2175ef14c6c25e274d006
Zandile_Mdiniso <132349003+ZandileMdiniso@users.noreply.github.com>
2023-09-01 11:16:56 +0200

    Update main.py

--- main.py
+++ main.py
@@ -17,15 +17,30 @@ scaler.fit(df[numeric_columns])
 # Streamlit app
 st.title("HOUSE VALUE PREDICTION")
 
-# User input for features
-st.header('Feature Input')
 feature1 = st.number_input("housing_median_age", value=0)
 feature2 = st.number_input("total_bedrooms", value=0)
 feature3 = st.number_input("households", value=0)
 feature4 = st.number_input("median_income", value=0)
+feature5 = st.selectbox("ocean_proximity", ["<1H OCEAN", "INLAND", "NEAR OCEAN", "NEAR BAY", "ISLAND"])
 
-# Scale and transform user input
-user_input = scaler.transform([[feature1, feature2, feature3, feature4]])
+# Button for predictions
+clicked = st.button('Get Predictions')
+
+# Perform predictions when the button is clicked
+if clicked:
+    # Preprocess the categorical feature
+    ocean_proximity_mapping = {
+        "<1H OCEAN": 0,
+        "INLAND": 1,
+        "NEAR OCEAN": 2,
+        "NEAR BAY": 3,
+        "ISLAND": 4
+    }
+    feature9_encoded = ocean_proximity_mapping[feature9]
+
+    # Prepare the input for prediction
+    input_features = np.array([[feature1, feature2, feature3, feature4, feature5]])
+
 
 # Button for predictions
 clicked = st.button('Get Predictions')
