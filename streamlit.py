5a77b847d4e1b5dbf03de69a654442ece0789252
Zandile_Mdiniso <132349003+ZandileMdiniso@users.noreply.github.com>
2023-09-01 11:04:36 +0200

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -1,39 +1,36 @@
-import streamlit as st
-import tensorflow as tf
-import numpy as np
-import pandas as pd
-
-# Load the pre-trained model
-model = tf.keras.models.load_model('model3.h5')
-
-df = pd.read_csv('/content/drive/MyDrive/houseing_clean.csv')
-
-# Preprocess user input
-scaler = StandardScaler()
-numeric_columns = ['housing_median_age', 'total_bedrooms', 'households', 'median_income']
-scaler.fit(df[numeric_columns])
-
-# Streamlit app
-st.title("HOUSE VALUE PREDICTION")
+# Streamlit UI
+st.title("HOUSE PRICE PREDICTION")
 
 # User input for features
 st.header('Feature Input')
+
 feature1 = st.number_input("housing_median_age", value=0)
 feature2 = st.number_input("total_bedrooms", value=0)
 feature3 = st.number_input("households", value=0)
 feature4 = st.number_input("median_income", value=0)
-
-# Scale and transform user input
-user_input = scaler.transform([[feature1, feature2, feature3, feature4]])
+feature5 = st.selectbox("ocean_proximity", ["<1H OCEAN", "INLAND", "NEAR OCEAN", "NEAR BAY", "ISLAND"])
 
 # Button for predictions
 clicked = st.button('Get Predictions')
 
 # Perform predictions when the button is clicked
 if clicked:
-    # Perform predictions using the pre-trained model
-    prediction = model.predict(user_input)
+    # Preprocess the categorical feature
+    ocean_proximity_mapping = {
+        "<1H OCEAN": 0,
+        "INLAND": 1,
+        "NEAR OCEAN": 2,
+        "NEAR BAY": 3,
+        "ISLAND": 4
+    }
+    feature9_encoded = ocean_proximity_mapping[feature9]
+
+    # Prepare the input for prediction
+    input_features = np.array([[feature1, feature2, feature3, feature4, feature5]])
+
+    # Perform predictions using the selected model
+    prediction = model.predict(input_features)
 
     # Display the prediction result
     st.header('Prediction')
-    st.write(f'The predicted median house value is: {prediction[0][0]}')
+    st.write(f'The predicted house price is: {prediction[0][0]}')
