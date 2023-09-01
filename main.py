1920a1d7c5c76bb9b3a4953cbc60351fec3fe9d1
Zandile_Mdiniso <132349003+ZandileMdiniso@users.noreply.github.com>
2023-09-01 11:13:15 +0200

    Update main.py

--- main.py
+++ main.py
@@ -5,10 +5,17 @@ import pandas as pd
 from sklearn.preprocessing import StandardScaler
 
 # Load the pre-trained model
-model = tf.keras.models.load_model('model3.h5')
+model = tf.keras.models.load_model('model3.pkl')
 
-# Streamlit UI
-st.title("HOUSE PRICE PREDICTION")
+df =  pd.read_csv('housing_clean.csv')
+
+# Preprocess user input
+scaler = StandardScaler()
+numeric_columns = ['housing_median_age', 'total_bedrooms', 'households', 'median_income']
+scaler.fit(df[numeric_columns])
+
+# Streamlit app
+st.title("HOUSE VALUE PREDICTION")
 
 # User input for features
 st.header('Feature Input')
@@ -17,7 +24,6 @@ feature2 = st.number_input("total_bedrooms", value=0)
 feature3 = st.number_input("households", value=0)
 feature4 = st.number_input("median_income", value=0)
 
-scaler = StandardScaler()
 # Scale and transform user input
 user_input = scaler.transform([[feature1, feature2, feature3, feature4]])
 
