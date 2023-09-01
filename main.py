1dcd98df179822f02a5bfb9181ee3af5aae8bcd3
Zandile_Mdiniso <132349003+ZandileMdiniso@users.noreply.github.com>
2023-09-01 11:17:51 +0200

    Update main.py

--- main.py
+++ main.py
@@ -36,7 +36,6 @@ if clicked:
         "NEAR BAY": 3,
         "ISLAND": 4
     }
-    feature9_encoded = ocean_proximity_mapping[feature9]
 
     # Prepare the input for prediction
     input_features = np.array([[feature1, feature2, feature3, feature4, feature5]])
